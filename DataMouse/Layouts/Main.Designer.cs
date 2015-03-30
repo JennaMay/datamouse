816621430a085c95b6cf52f2936f43941e079f44
Jenna May <jenna.lee85@yahoo.com>
2015-03-30 14:28:24 -0400

    moving repositories (init)

---
+++ DataMouse/Layouts/Main.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace DataMouse.Layouts
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.profilesComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // profilesComboBox
+            // 
+            this.profilesComboBox.FormattingEnabled = true;
+            this.profilesComboBox.Location = new System.Drawing.Point(83, 104);
+            this.profilesComboBox.Name = "profilesComboBox";
+            this.profilesComboBox.Size = new System.Drawing.Size(121, 23);
+            this.profilesComboBox.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(884, 411);
+            this.Controls.Add(this.profilesComboBox);
+            this.Font = new System.Drawing.Font("Moire", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DataMouse";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox profilesComboBox;
+    }
+}
---
+++ DataMouse/Layouts/Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace DataMouse.Layouts
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string[] lineOfContents = File.ReadAllLines("profiles.txt");
+            foreach (var line in lineOfContents)
+            {
+                string[] tokens = line.Split('#');
+                profilesComboBox.Items.Add(tokens[0]);
+            }
+        }
+    }
+}
